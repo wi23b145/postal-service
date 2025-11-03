405fae736210688596b974d13c20d139ef835592
wi23b145 <wi23b145@technikum-wien.at>
2025-11-03 20:12:33 +0100

    Final Version 2.0

--- letter-worker/src/main/java/org/example/config/RabbitConfig.java
+++ letter-worker/src/main/java/org/example/config/RabbitConfig.java
@@ -1,21 +1,20 @@
 
 package org.example.config;
 
-import org.example.repo.LetterRepository;
-import org.example.worker.LetterListener;
 import org.springframework.amqp.core.Queue;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class RabbitConfig {
+    //@Primary
 
-    @Bean
     public Queue letterQueue(@Value("${app.queues.letter}") String name) {
-
         return new Queue(name, true);
     }
+
 
 
 }
